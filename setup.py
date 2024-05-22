ed7dc81fb1fc610316ab7a1b8323db546ee7933c
Carsten Jørgensen <carstenj@gmail.com>
2024-05-22 17:28:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,9 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=required,
+    install_requires=[
+       'PYMC==5.15.0'
+    ],
     license="MIT",
     url="https://github.com/carsten-j/quad5",
 )
